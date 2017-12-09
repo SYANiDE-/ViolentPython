a6d6844bec274608b2a6eeb1a2cd93fad0020900
Chase Hatch <chase.hatch.business@gmail.com>
2017-12-09 00:09:20 -0800

    added IP geolocation tool

---
+++ NTA_geoIP.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+import pygeoip, sys
+
+
+geo = pygeoip.GeoIP('/opt/GeoIP/Geo.dat')
+def q(targ):
+    r = geo.record_by_name(targ)
+    # print(str(r))
+    print("[+] Target: %s : %s : %s: lat:%s lon:%s" % (targ, str(r['metro_code']), str(r['country_name']), str(r['latitude']), str(r['longitude'])))
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("USAGE: %s %s" % (sys.argv[0], "[IP]"))
+    else:
+        try:
+            q(sys.argv[1])
+        except Exception, X:
+            print(str(X))
+    
+
+if __name__=="__main__":
+    main()
+
+
+
