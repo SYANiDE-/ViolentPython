d3d7623d4bcc2842c63dcba9cc8424c433aa1d0a
Chase Hatch <chase.hatch.business@gmail.com>
2017-12-20 18:42:04 -0800

    added arbitrary port selection, NTA_tcp_seq_nums1.py

--- NTA_tcp_seq_nums1.py
+++ NTA_tcp_seq_nums1.py
@@ -3,14 +3,19 @@ from scapy.all import *
 import sys
 
 
-def guessSeqNumTCP(targ):
+def guessSeqNumTCP(**kwargs):
+    targ = kwargs['targ']
+    if 'dport' in kwargs.keys():
+        dport = kwargs['dport']
+    else:
+        dport = 80
     sn = 0
     pn = 0
     diff = 0
     for i in range(1,5):
         if pn != 0:
             pn = sn
-        pkt = IP(dst=targ) / TCP()
+        pkt = IP(dst=targ) / TCP(dport=dport)
         ans = sr1(pkt, verbose=0)
         sn = ans.getlayer(TCP).seq
         diff = sn - pn
@@ -19,12 +24,17 @@ def guessSeqNumTCP(targ):
 
 
 def main():
-    if len(sys.argv) <> 2:
-        print("USAGE: %s %s" % (sys.argv[0], "[target IP]"))
+    if not len(sys.argv) >= 2:
+        print("USAGE: %s %s %s" % (sys.argv[0], "[target IP]", "[optional: dst port]"))
         sys.exit
     else:
-        targ = sys.argv[1]
-        sn = guessSeqNumTCP(targ)
+        kwargs = {}
+        kwargs['targ'] = sys.argv[1]
+        try:
+            kwargs['dport'] = int(sys.argv[2])
+        except:
+            pass
+        sn = guessSeqNumTCP(**kwargs)
         print("[+] TCP_SEQ Predict (future ~ +150 ACK): %d" %(sn))
 
 
